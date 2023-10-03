13e2bf3a44b2f51bd365ab8e5f0cc4f9f60e81ad
erik <erik.mulder@smhi.se>
2023-10-03 18:44:28 +0200

    dat tramp ding is wel belangrijk

--- .startupscript.erik.sh
+++ .startupscript.erik.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
 # set keyboard delay and rate
-xset r rate 200 30 &
+gsettings set org.gnome.desktop.peripherals.keyboard delay 200
+gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 30
+
+# set check-alive-timeout
+gsettings set org.gnome.mutter check-alive-timeout 15000
 
 # synclient... alleen X11?
-synclient ClickFinger2=2
-synclient TapButton2=2
+# synclient ClickFinger2=2
+# synclient TapButton2=2
 
-synclient ClickFinger3=3
-synclient TapButton3=3
+# synclient ClickFinger3=3
+# synclient TapButton3=3
 
 # adjust font scaling factor based on display
 primaryDisp=$(xrandr -q | grep primary)
@@ -16,14 +20,14 @@ primaryDisp=$(xrandr -q | grep primary)
 if [[ $primaryDisp == *"eDP-1"* ]];
 then
     #internal
-    gsettings set org.gnome.desktop.interface text-scaling-factor 1.0
+    gsettings set org.gnome.desktop.interface text-scaling-factor 1.1
 else
     #external
-    gsettings set org.gnome.desktop.interface text-scaling-factor 0.85
+    gsettings set org.gnome.desktop.interface text-scaling-factor 1.0
 fi
 
 # poging om headerbar uit te zetten in terminal
-gsettings set org.gnome.Terminal.Legacy.Settings headerbar false
+# gsettings set org.gnome.Terminal.Legacy.Settings headerbar false
 
 # alt-tab behavior
 gsettings set org.gnome.desktop.wm.keybindings switch-applications "['<Super>Tab']"
@@ -31,3 +35,7 @@ gsettings set org.gnome.desktop.wm.keybindings switch-windows "['<Alt>Tab']"
 gsettings set org.gnome.desktop.wm.keybindings switch-applications-backward "['<Shift><Super>Tab']"
 gsettings set org.gnome.desktop.wm.keybindings switch-windows-backward "['<Shift><Alt>Tab']"
 gsettings set org.gnome.shell.window-switcher current-workspace-only "true"
+
+
+# conky
+conky -c ~/.conkyrc
